46bf1fe287628b39779d5d284f4de0a8fd466af1
pravinshedbale1 <pravin.shedbale@outlook.com>
2024-07-28 21:36:23 +0530

    foundations

---
+++ 1_Foundations/Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
