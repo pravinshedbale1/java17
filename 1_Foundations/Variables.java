fa042547dd4279d8fd1c52a107b1dd1c48a9edde
pravinshedbale1 <pravin.shedbale@outlook.com>
2024-07-28 21:21:28 +0530

    foundations

---
+++ 1_Foundations/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte byteVal = 127;
+        short shortVal = 32767;
+        int intVal = 10;
+        long longVal = 9223372036854775807L;
+        float floatVal = 10.7f;
+        double doubleVal = 19822d;
+        boolean booleanVal = true;
+        char charValue = 'A';
+
+        System.out.println(byteVal);
+        System.out.println(shortVal);
+        System.out.println(intVal);
+        System.out.println(longVal);
+        System.out.println(floatVal);
+        System.out.println(doubleVal);
+        System.out.println(booleanVal);
+        System.out.println(charValue);
+    }
+}
